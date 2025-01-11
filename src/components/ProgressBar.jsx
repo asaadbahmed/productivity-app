21dbdf1487223bea879b25bfef7cf7aaa68ea9e8
Asaad Ahmed <asaadbinahmed@gmail.com>
2025-01-11 00:19:09 -0500

    uninstalled mui & removed commented code from progbar

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,30 +1,3 @@
 import { Progress } from "@/components/ui/progress";
 const ProgressBar = ({ value }) => <Progress value={value} />;
 export default ProgressBar;
-/*
-import { styled } from "@mui/material/styles";
-import LinearProgress, {
-  linearProgressClasses,
-} from "@mui/material/LinearProgress";
-
-const ProgressBar = styled(LinearProgress)(({ theme }) => ({
-  height: 10,
-  borderRadius: 5,
-  [`&.${linearProgressClasses.colorPrimary}`]: {
-    backgroundColor: theme.palette.grey[200],
-    ...theme.applyStyles("light", {
-      backgroundColor: theme.palette.grey[800],
-    }),
-  },
-  [`& .${linearProgressClasses.bar}`]: {
-    borderRadius: 5,
-    backgroundColor: "#00FF96",
-    ...theme.applyStyles("light", {
-      backgroundColor: "#308fe8",
-    }),
-  },
-}));
-
-export default ProgressBar;
-
-*/
